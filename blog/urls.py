86f69afdb6a14e85e3902f85ea47c68a444c4abc
Yuri Souza <yurisouza1712@gmail.com>
2025-01-21 20:12:41 -0300

    Added style and a view for my home page blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
-  path('', views.blog_list, name='home'),
-  path('post/<int:pk>/', views.post_detail, name='post_detail'),
+  path('', views.post_list, name='post_list'),
+  path('<slug:slug>/', views.post_detail, name='post_detail'),
+  # path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
 if settings.DEBUG:
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render
-from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+from django.core.paginator import Paginator
 
-def blog_list(request):
-    tag = request.GET.get('tag')  # Get tag from query parameters
+def post_list(request):
+    tag = request.GET.get('tag')
+    posts = Post.objects.filter(status='published').order_by('-published_at')
     if tag:
-        posts = Post.objects.filter(tags__name__in=[tag])
-    else:
-        posts = Post.objects.all()
-    return render(request, 'blog_list.html', {'posts': posts})
+        posts = posts.filter(tags__name=tag)
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    # Paginate posts (10 per page)
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/post_list.html', {'posts': page_obj})
+
+
+def post_detail(request, pk, slug):
+    post = get_object_or_404(Post, pk=pk, slug=slug, status='published')
     post.views += 1  # Increment the views count
     post.save()  # Save the updated view count to the database
     return render(request, 'post_detail.html', {'post': post})
